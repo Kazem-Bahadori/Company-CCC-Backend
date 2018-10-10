83129f23a6c63d10cbeb24c5028bf15306b97c78
Simon Rynningsjö <simry196@student.liu.se>
2018-10-10 12:10:27 +0200

    Refactor the twitch api calls

--- backend/src/Machinepacks/machinepack-c3twitch/machines/filters.js
+++ backend/src/Machinepacks/machinepack-c3twitch/machines/filters.js
@@ -6,22 +6,22 @@ module.exports = {
   cacheable: false,
   sync: false,
   inputs: {
+    assetType: {
+      example: 'top',
+      description: 'amount of what you want to get',
+      require: false
+      },
       filterType: {
         example: 'games',
         description: 'what you want to get',
         require: true
       },
-      assetType: {
-        example: 'top',
-        description: 'amount of what you want to get',
-        require: false
-      },
-      additionalFilter: {
+      filterValue: {
         example: 'first',
         description: 'additional filter for calls',
         require: false
       },
-      amount: {
+      context: {
         example: '20',
         description: 'amount of items to call',
         require: false
@@ -38,23 +38,59 @@ module.exports = {
   ) {
     console.log('Twitch function triggered!')
 
-    var url = "https://api.twitch.tv/helix/" + inputs.query.filterType;
+    let url = 'https://api.twitch.tv/helix/';
+    const amount = 10;
+    
+    console.log(inputs.query)
 
-    if (inputs.query.assetType != null){
-      url = url.concat("/" + inputs.query.assetType);
-    }
-    if (inputs.query.additionalFilter != null){
-      url = url.concat("?" + inputs.query.additionalFilter);
-    }
-    if (inputs.query.amount != null){
-      url = url.concat("=" + inputs.query.amount);
+    if (inputs.query.assetType == 'games'){
+      if (inputs.query.filterType == 'top'){
+        url = url.concat('games/top')
+      } 
+    } else if (inputs.query.assetType == 'streams'){
+      if (inputs.query.filterType == 'game_id'){
+        if(inputs.query.filterValue != null) {
+          url = url.concat('streams?game_id=' + inputs.query.filterValue)
+          fetchFromTwitch(url)
+            .then(response => {
+              // let streams = []
+              // for (stream of response) {
+              //   streams.add(stream.game_id);
+              // }
+               return response;
+            })
+            .then(x => console.log(x));
+        }
+        
+      }
     }
 
-    console.log(url)
 
-         fetch(url, {headers: {"Client-ID": '3jxj3x3uo4h6xcxh2o120cu5wehsab'}})
-         .then(function(response){
-             return exits.success(response.json());
-         })
+
+    
+    // if (inputs.query.assetType != null){
+    //   url = url.concat('/' + inputs.query.assetType);
+    // }
+    // if (inputs.query.additionalFilter != null){
+    //   url = url.concat('?' + inputs.query.additionalFilter);
+    // }
+    // if (inputs.query.amount != null){
+    //   url = url.concat('=' + inputs.query.amount);
+    // }
+
+    function fetchFromTwitch(url) {
+      return new Promise(function(resolve, reject){
+        fetch(url, {headers: {'Client-ID': '3jxj3x3uo4h6xcxh2o120cu5wehsab'}})
+        .then(function(response){
+            resolve(response.json());
+        })
+      });
+    }
+
+    fetch(url, {headers: {'Client-ID': '3jxj3x3uo4h6xcxh2o120cu5wehsab'}})
+        .then(response => {
+            return exits.success(response.json());
+        })
+
   },
 };
