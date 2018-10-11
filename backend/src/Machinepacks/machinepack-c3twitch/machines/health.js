8803ad1b395fe3d520eb0b91917be058a3c00a40
Rickard Hellenberg <riche828@student.liu.se>
2018-10-11 17:05:31 +0200

    implemented the health api-call

--- backend/src/Machinepacks/machinepack-c3twitch/machines/health.js
+++ backend/src/Machinepacks/machinepack-c3twitch/machines/health.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 module.exports = {
 
 
@@ -28,12 +30,48 @@ module.exports = {
   },
 
 
-  fn: function(inputs, exits
+  fn: function (inputs, exits
     /*``*/
   ) {
-    return exits.success();
-  },
 
+    //Only for testing purpose
+    const mockFetch = new Promise(function (resolve, reject) {
+      setTimeout(() => {
+        const response = {
+          data: 'exists'
+        };
+        resolve(response);
+        //reject is for testing if api call does not work
+        //reject(404);
+      }, 300);
+    });
 
+    const fetchFromTwitch = new Promise(function (resolve, reject) {
+      let url = 'https://api.twitch.tv/helix/streams';
+      fetch(url, {
+          headers: {
+            'Client-ID': '3jxj3x3uo4h6xcxh2o120cu5wehsab'
+          }
+        })
+        .then(response => response.json())
+        .then(response => {
+          if (response.data != null) {
+            resolve();
+          } else {
+            const status = (response.status != null) ? response.status : 500;
+            reject(status);
+          }
+        })
+    });
 
-};
+    function ping(fetchFunction) {
+      return new Promise(function (resolve, reject) {
+        fetchFunction.then(value => {
+            resolve(value);
+          })
+          .catch(err => reject(err));
+      });
+    }
+    return exits.success(ping(fetchFromTwitch));
+  }
+}
--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,5 +1,9 @@
-import { version } from '../../package.json';
-import { Router } from 'express';
+import {
+	version
+} from '../../package.json';
+import {
+	Router
+} from 'express';
 const fetch = require('node-fetch');
 const Twitch = require('../Machinepacks/machinepack-c3twitch');
 //Steam node-machine from npm
@@ -15,16 +19,36 @@ export default ({
 			version
 		});
 	});
+	api.get('/twitch/health', (req, res) => {
+		Twitch.health().exec({
+
+			// An unexpected error occurred.
+			error: function (err) {
+
+				console.log(err);
+				res.sendStatus(500);
+			},
+			// OK.
+			success: function (result) {
+
+				result
+					.then(() => {
+						res.sendStatus(200);
+					})
+					.catch(err => res.sendStatus(err));
+			},
+		});
+	});
 	api.get('/twitch/filters', (req, res) => {
 		const inputs = {
 			query: req.query,
 			body: req.body,
 		}
 		Twitch.filters(inputs).exec({
-			
+
 			// An unexpected error occurred.
 			error: function (err) {
-				
+
 				console.log(err);
 				res.sendStatus(500);
 			},
@@ -32,11 +56,35 @@ export default ({
 			success: function (result) {
 
 				result.then(function (response) {
-					res.send(response); 
-					});
+					res.send(response);
+				});
 			},
 		});
 	});
+
+	api.get('/twitch/search', (req, res) => {
+		const inputs = {
+			query: req.query,
+			body: req.body,
+		}
+		Twitch.filters(inputs).exec({
+
+			// An unexpected error occurred.
+			error: function (err) {
+
+				console.log(err);
+				res.sendStatus(500);
+			},
+			// OK.
+			success: function (result) {
+
+				result.then(function (response) {
+					res.send(response);
+				});
+			},
+		});
+	});
+
 	api.get('/steam/filters', (req, res) => {
 		// Returns on global statistics of a specific game
 		Steam.getGlobalStatsForGame({
