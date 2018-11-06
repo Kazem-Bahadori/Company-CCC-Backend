2cd6df9626688c40c8320d57b06ef0bdcbed4821
Rickard Hellenberg <riche828@student.liu.se>
2018-11-06 17:00:06 +0100

    bug-fix when missing filterValue

--- backend/src/Machinepacks/machinepack-c3twitch/machines/filters.js
+++ backend/src/Machinepacks/machinepack-c3twitch/machines/filters.js
@@ -45,30 +45,14 @@ module.exports = {
 
     if (inputs.query.assetType == 'games') {
       if (inputs.query.filterType == 'top') {
-        if (inputs.query.filterValue != undefined) {
-          if (inputs.query.filterValue >= 1 && inputs.query.filterValue <= 100) { // each twitch-call only accepts numbers between 1-100.
-            url = url.concat('games/top?first=' + inputs.query.filterValue)
-            fetchFromTwitch(url) //gets the top streamed games on twitch. 
-              .then(response => {
-                gamesIsOnSale(response.data)
-                  .then(res => {
-                    return exits.success(res);
-                  });
-              });
-          } else {
-            return exits.error('bad request - filtervalue for top games must be between 1-100')
-          }
-
-        } else {
-          url = url.concat('games/top')
-          fetchFromTwitch(url) //gets the top streamed games on twitch. 20 is default in the twitch api
-            .then(response => {
-              fetchSteamData(response.data[0].name)
-                .then(name => {
-                  return exits.success(response);
-                });
-            });
-        }
+        const amount = inputs.query.filterValue != undefined ? inputs.query.filterValue : 20;
+        getTopGames(amount)
+          .then(x => {
+            return exits.success(x);
+          })
+          .catch(err => {
+            return exits.error(err);
+          });
       } else {
         return exits.error('bad request - filterType input error');
       }
@@ -110,6 +94,22 @@ module.exports = {
       }
     } else {
       return exits.error('bad request - assetType input error');
+    }
+
+    function getTopGames(amount) {
+      return new Promise((resolve, reject) => {
+        if (amount >= 1 && amount <= 100) { // each twitch-call only accepts numbers between 1-100.
+          url = url.concat('games/top?first=' + amount)
+          fetchFromTwitch(url) //gets the top streamed games on twitch. 
+            .then(response => {
+              gamesIsOnSale(response.data)
+                .then(res => resolve(res))
+                .catch(err => reject(err));
+            });
+        } else {
+          return exits.error('bad request - filtervalue for top games must be between 1-100')
+        }
+      });
     }
 
     function fetchFromTwitch(url) {
@@ -190,7 +190,7 @@ module.exports = {
           },
           // OK.
           success: function (result) {
-            resolve({'appid': appId, price: result});
+            resolve({ 'appid': appId, price: result });
           },
         });
       });
