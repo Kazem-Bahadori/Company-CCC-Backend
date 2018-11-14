fcdb74de38387bda6f1d66b85644b47c45ebe4c0
osclo118 <osclo118@student.liu.se>
2018-11-14 09:10:28 +0100

    minior updates

--- backend/src/Machinepacks/machinepack-c3twitch/machines/search.js
+++ backend/src/Machinepacks/machinepack-c3twitch/machines/search.js
@@ -78,46 +78,6 @@ module.exports = {
       return exits.error('bad request - incorrect assetType')
     }
 
-
-    /*
-    Hjälp för hur man arbetar med javascript
-
-    //Hur man definierar variabler i modern javascript
-    let toChange = "unchanged";
-    const cannotBeChanged = 0;
-
-    // Hur man skickar med parametrar till en promise
-    function timeOut(time, value) {
-      return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-          toChange = value;
-          resolve(toChange);
-        }, time);
-      });
-    }
-
-    //Ett sätt att vänta på massa anrop är klara innan man skriver ut värdet
-    Promise.all([timeOut(1000, 'hej'), promise1, promise1])
-      .then(values => {
-        console.log(values);
-      });
-    
-    //VAD BETYDER PILEN?!?!?
-    promise1
-      .then(value => {
-        return value;
-        //console.log(value);
-        // expected output: "foo"
-      });
-
-    //Pilen är ett snabbare (coolare) sätt att skriva detta
-    promise1
-      .then(function (value) {
-        console.log(value);
-        return exits.success(value);
-      });
-    console.log(toChange);
-    */
     function searchOnTwitch(url) { //Sends the url with the id 
       return new Promise(function (resolve, reject) {
         fetch(url, { headers: { 'Accept': 'application/vnd.twitchtv.v5+json',
