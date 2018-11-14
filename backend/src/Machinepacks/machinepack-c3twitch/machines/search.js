1863367ed5c5025c45e4f81ab4530d03c779e37f
Oscar Hubertsson <oschu750@student.liu.se>
2018-11-14 11:49:47 +0100

    kan hämta streams och games nu via search

--- backend/src/Machinepacks/machinepack-c3twitch/machines/search.js
+++ backend/src/Machinepacks/machinepack-c3twitch/machines/search.js
@@ -35,7 +35,7 @@ module.exports = {
     /*``*/
   ) {
     let url = 'https://api.twitch.tv/kraken/search/'
-/*
+
     if (inputs.query.assetType == "games"){ // if you are searching for games on twitch
       if (inputs.query.filterValue != undefined){ //Checks that filtervalue isn't left empty
         url = url.concat('games?query=' + inputs.query.filterValue) //adds the searchword to the url
@@ -53,12 +53,29 @@ module.exports = {
       } else {
         return exits.error('bad request - no filterValue given')
       }
+    } else if(inputs.query.assetType == "streams"){
+      if (inputs.query.filterValue != undefined){ //Checks that filtervalue isn't left empty
+        url = url.concat('streams?query=' + inputs.query.filterValue) //adds the searchword to the url
+        searchOnTwitch(url) //calls the searchOnTwitch function
+        .then( response => { //takes the response from the searchOnTwitch function
+
+       /*   response.streams.forEach(function(element) { //Changes name from _id to id
+            element.id = element._id;
+            delete element._id;
+
+          }); */
+
+          return exits.success(response);  // returns the response to the client
+        })
+      } else {
+        return exits.error('bad request - no filterValue given')
+      }
     } else {
       return exits.error('bad request - incorrect assetType')
     }
-*/
 
-  /* url = 'https://api.twitch.tv/kraken/search'*/
+
+  /* url = 'https://api.twitch.tv/kraken/search'
    if (inputs.query.assetType == "streams"){ // if you are searching for games on twitch
      if (inputs.query.filterValue != undefined){ //Checks that filtervalue isn't left empty
        url = url.concat('streams?query=' + inputs.query.filterValue) //adds the searchword to the url
@@ -69,7 +86,7 @@ module.exports = {
            element.id = element._id;
            delete element._id;
 
-         }); */
+         });
 
          return exits.success(response);  // returns the response to the client
        })
@@ -78,7 +95,7 @@ module.exports = {
      }
    } else {
      return exits.error('bad request - incorrect assetType')
-   }
+   }*/
 
     /*
     Hjälp för hur man arbetar med javascript
