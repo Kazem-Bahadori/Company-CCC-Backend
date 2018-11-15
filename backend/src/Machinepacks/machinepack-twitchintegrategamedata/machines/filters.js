80b4863bdf5d1e80f164c8e16e22396f96fe03c7
Simon Rynningsjö <simry196@student.liu.se>
2018-11-15 16:03:08 +0100

    Minor code structure fix

--- backend/src/Machinepacks/machinepack-twitchintegrategamedata/machines/filters.js
+++ backend/src/Machinepacks/machinepack-twitchintegrategamedata/machines/filters.js
@@ -64,6 +64,11 @@ module.exports = {
       }
     }
 
+    //------------------------------------- Seperate functions ---------------------------------------------------------------
+
+    /*
+      
+    */
     function getTopGames(amount) {
       return new Promise((resolve, reject) => {
         if (amount >= 1 && amount <= 100) { // each twitch-call only accepts numbers between 1-100.
@@ -75,111 +80,116 @@ module.exports = {
             }
           }
           return new Promise((resolve, reject) => {
-          Twitch.filters(inputs).exec({
-            // An unexpected error occurred.
-            error: function (err) {
-              reject(err);
-            },
-            // OK.
-            success: function (result) {
+            Twitch.filters(inputs).exec({
+              // An unexpected error occurred.
+              error: function (err) {
+                reject(err);
+              },
+              // OK.
+              success: function (result) {
 
-              resolve(result)
-              //return exits.success(result);
+                resolve(result)
+                //return exits.success(result);
 
-            },
-          });
-        })
+              },
+            });
+          })
             .then(response => {
               gamesIsOnSale(response.data)
-                
+
                 .then(res => resolve(res))
                 .catch(err => reject(err));
             });
-          
+
         } else {
           return exits.error('bad request - filtervalue for top games must be between 1-100')
         }
-      });
-
-      //denna function är vad jag behöver för att kalla twitch machine
-      function getSteamID(nameOfGame) {
-        const inputs = {
-          query: {
-            assetType: 'games',
-            filterType: 'on_twitch',
-            filterValue: nameOfGame
-          }
-        }
-        return new Promise((resolve, reject) => {
-          Steam.filters(inputs).exec({
-            // An unexpected error occurred.
-            error: function (err) {
-              reject(err);
-            },
-            // OK.
-            success: function (result) {
-              resolve(result);
-            },
-          });
-        });
-      }
-
-      function gamesIsOnSale(twitchGames) {
-        const promises = [];
-        const games = twitchGames;
-        games.forEach(function (element) {
-          promises.push(new Promise(function (resolve, reject) {
-            getSteamID(element.name)
-              .then(game => {
-                if (game.appId != undefined) {
-                  getSteamData(game.appId)
-                    .then(data => {
-                      resolve(data)
-                    })
-                    .catch(() => resolve(false));
-                } else {
-                  resolve(game);
-                }
-              });
-          })
-          );
-        });
-
-        return new Promise((resolve, reject) => {
-          Promise.all(promises).then(values => {
-            for (let i = 0; i < games.length; i++) {
-              games[i]['steam'] = values[i];
-            }
-            resolve(games);
-          })
-        });
-      }
-
-      function getSteamData(appId) {
-        const inputs = {
-          query: {
-            assetType: 'price',
-            filterType: 'app_id',
-            filterValue: appId
-          }
-        }
-        return new Promise((resolve, reject) => {
-          Steam.filters(inputs).exec({
-            // An unexpected error occurred.
-            error: function (err) {
-              reject(err);
-            },
-            // OK.
-            success: function (result) {
-              resolve({ 'appid': appId, price: result });
-            },
-          });
-        });
-      }
+      })
     }
 
+
+    /*
+    
+    */
+    function getSteamID(nameOfGame) {
+      const inputs = {
+        query: {
+          assetType: 'games',
+          filterType: 'on_twitch',
+          filterValue: nameOfGame
+        }
+      }
+      return new Promise((resolve, reject) => {
+        Steam.filters(inputs).exec({
+          // An unexpected error occurred.
+          error: function (err) {
+            reject(err);
+          },
+          // OK.
+          success: function (result) {
+            resolve(result);
+          },
+        });
+      });
+    }
+
+    /*
+      
+    */
+    function gamesIsOnSale(twitchGames) {
+      const promises = [];
+      const games = twitchGames;
+      games.forEach(function (element) {
+        promises.push(new Promise(function (resolve, reject) {
+          getSteamID(element.name)
+            .then(game => {
+              if (game.appId != undefined) {
+                getSteamData(game.appId)
+                  .then(data => {
+                    resolve(data)
+                  })
+                  .catch(() => resolve(false));
+              } else {
+                resolve(game);
+              }
+            });
+        })
+        );
+      });
+
+      return new Promise((resolve, reject) => {
+        Promise.all(promises).then(values => {
+          for (let i = 0; i < games.length; i++) {
+            games[i]['steam'] = values[i];
+          }
+          resolve(games);
+        })
+      });
+    }
+
+    /*
+      
+    */
+    function getSteamData(appId) {
+      const inputs = {
+        query: {
+          assetType: 'price',
+          filterType: 'app_id',
+          filterValue: appId
+        }
+      }
+      return new Promise((resolve, reject) => {
+        Steam.filters(inputs).exec({
+          // An unexpected error occurred.
+          error: function (err) {
+            reject(err);
+          },
+          // OK.
+          success: function (result) {
+            resolve({ 'appid': appId, price: result });
+          },
+        });
+      });
+    }
   },
-
-
-
 };
