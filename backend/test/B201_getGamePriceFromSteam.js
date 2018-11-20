1f3469d46c0e6adfdad6c4eb3e622f6ae9ecd8c4
hermansvensk <herman.svensk@gmail.com>
2018-11-20 09:28:02 +0100

    first unit test working

--- backend/test/B201_getGamePriceFromSteam.js
+++ backend/test/B201_getGamePriceFromSteam.js
@@ -4,24 +4,40 @@ import Steam from '../src/Machinepacks/machinepack-c3steam';
 
 describe('B201 TEST', () =>{
     it('Testing game price from Steam', ()=> {
+    let ok = false;
+
 
         const inputs = {
           query: {assetType: 'price', filterType: 'app_id', filterValue: '57690'},
         }
 
-        Steam.filters(inputs).exec({
+        return new Promise(function(resolve, reject){
+          Steam.filters(inputs).exec({
+            // An unexpected error occurred.
+            error: function (err) {
 
-          // An unexpected error occurred.
-          error: function (err) {
+              console.log(err);
+            },
+            // OK.
+            success: function (result) {
 
-            console.log(err);
-          },
-          // OK.
-          success: function (result) {
-            console.log(result.final_formatted);
-            assert.property(result, 'final_formatted1')
-
-          },
+              console.log(result);
+              console.log('innan');
+              console.log(ok);
+              if(result.hasOwnProperty('final_formatted')){
+                ok = true
+              }
+              console.log(ok);
+              console.log('efter');
+              resolve(ok);
+            },
+          });
+        })
+        .then((result) =>{
+          assert.isTrue(ok);
+        })
+        .catch((error) => {
+          assert.isNotOk(error);
         });
     });
   });
