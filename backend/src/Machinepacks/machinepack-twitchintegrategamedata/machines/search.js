e122c88df9973dd8ce26465dcaf707a9adef69e9
osclo118 <osclo118@student.liu.se>
2018-11-21 10:47:57 +0100

    searching for steam name

--- backend/src/Machinepacks/machinepack-twitchintegrategamedata/machines/search.js
+++ backend/src/Machinepacks/machinepack-twitchintegrategamedata/machines/search.js
@@ -38,10 +38,56 @@ module.exports = {
     if (inputs.query.assetType == "games") { // if you are searching for games on twitch
       if (inputs.query.queryString != undefined) {
 
+
+        return new Promise((resolve, reject) => {
+          Twitch.search(inputs).exec({
+            // An unexpected error occurred.
+            error: function (err) {
+              reject(err);
+            },
+            // OK.
+            success: function (result) {
+
+              resolve(result)
+              //console.log(result);
+              //return exits.success(result);
+
+            },
+          });
+        }) 
+        .then (response => {
+
+          response.games.forEach(element => {
+            let steamInputs = {
+              query: {
+                assetType: 'gameId',
+                queryString: element.name
+              }
+            }
+            new Promise((resolve, reject) => {
+              Steam.search(steamInputs).exec({
+                // An unexpected error occurred.
+                error: function (err) {
+                  reject(err);
+                  console.log(err);
+                },
+                // OK.
+                success: function (result) {
+                  //resolve({ 'appid': appId, price: result });
+                  //console.log(result);
+                  if (result.appId != 0) {
+                    element.steamId = result.appId;
+                  }
+          
+            
+          });
+        })
+
+
           //console.log("Test Oscar 2");
           //console.log(inputs);
-
-          return exits.success( new Promise((resolve, reject) => {
+         
+          /* let result = new Promise((resolve, reject) => {
             Twitch.search(inputs).exec({
               // An unexpected error occurred.
               error: function (err) {
@@ -51,6 +97,7 @@ module.exports = {
               success: function (result) {
   
                 //resolve(result)
+                
                 result.games.forEach(element => {
                   let steamInputs = {
                     query: {
@@ -58,7 +105,8 @@ module.exports = {
                       queryString: element.name
                     }
                   }
-                  return new Promise((resolve, reject) => {
+                  //console.log(element);
+                   new Promise((resolve, reject) => {
                     Steam.search(steamInputs).exec({
                       // An unexpected error occurred.
                       error: function (err) {
@@ -72,21 +120,21 @@ module.exports = {
                         if (result.appId != 0) {
                           element.steamId = result.appId;
                         }
-                        //console.log(element);
+                        console.log(element);
                       },
                     });
-                  });
-                });
-                //console.log(result);
-                //return exits.success(result);
-  
+                  }); 
+                });             
               },
-            });
-          }))
-
+            })
+            //resolve(result)
+          }) */
+          //console.log("result: " + result);        
+          
+          //return exits.success(result);
         //return exits.success();
       }
     }
-  },
+  }
 
 };
