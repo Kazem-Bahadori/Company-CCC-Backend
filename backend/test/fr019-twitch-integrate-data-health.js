5cb5ed9a38e5b080cb9576a7dd68e6dc76cdc55c
hermansvensk <herman.svensk@gmail.com>
2018-12-03 16:14:00 +0100

    added more api tests in postman

---
+++ backend/test/fr019-twitch-integrate-data-health.js
@@ -0,0 +1,29 @@
+let propertyExists = true;
+const assert = require('chai').assert; //bringring in the chai library
+const expect = require('chai').expect; //bringring in the chai library
+import GameData from '../src/Machinepacks/machinepack-twitchintegrategamedata';
+
+describe('fr019-twitch-integrate-data-health', () =>{
+
+  it('Twitch integrate data health', ()=> {
+    let ok = true;
+    return new Promise(function(resolve, reject){
+      GameData.health().exec({
+        error: function (error) {
+          reject(error)
+        },
+        success: function (result) {
+          if(result == null || result == undefined){
+            ok = false;
+          }
+          resolve(ok);
+        },
+      });
+    })
+    .then((ok) =>{
+    })
+    .catch((error) => {
+      expect(error).to.equal("bad request - filterValue input error");
+    });
+});
+});
